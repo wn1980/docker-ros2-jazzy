312d3aae08f13dd4e2a7925b60051dfd4889a224
wn1980 <waipotn@hotmail.com>
2025-04-06 20:59:46 +0700

    add description

--- hardware/kobuki/image/app/launch/description.launch.py
+++ hardware/kobuki/image/app/launch/description.launch.py
@@ -10,7 +10,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     
     urdf_path = PathJoinSubstitution(
-        [FindPackageShare("kobuki_description"), "urdf", "kobuki_standalone.urdf.xacro"]
+        [FindPackageShare("kobuki_description"), "urdf", "kobuki_base.urdf.xacro"]
     )
 
     return LaunchDescription([
